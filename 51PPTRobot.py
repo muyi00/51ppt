8782b9c80b05db66948ae75a9e7c01bacc704747
muyi00 <253682259@qq.com>
2019-01-11 20:10:46 +0800

    增加下载
    Co-Authored-By: muyi00 <253682259@qq.com>

--- 51PPTRobot.py
+++ 51PPTRobot.py
@@ -69,6 +69,7 @@ def getChinese(s):
     
 def progressBar(title,total,progress):
     done = int(50 * progress / total)
+    sys.stdout.flush()
     sys.stdout.write("\r[%s] [%s%s] %d%%" % (title,'#' * done, ' ' * (50 - done), 100 * progress / total))
     sys.stdout.flush()
 
@@ -137,29 +138,93 @@ def getDownloadUrl(url_path):
     return jointPath(base_51_url,'%s/%s/%s' % (path_list[0],path_list[1],path))
 
 
+def getPptFilePathName(dirNmae,name,suffix):
+    return '%s/%s.%s' % (dirNmae,name,suffix)
+
+def writelog(url_str ,name,e_str):
+    with open(jointPath(getBaseDir(),'log.txt'), 'a+') as f:
+        f.write('%s#%s#%s\n' % (url_str,name,e_str))
+
+def downloadFile(index,dirNmae,name,download_url):
+    """ 下载文件 """
+    filePathName = getPptFilePathName(dirNmae,name,'zip')
+    if os.path.isfile(filePathName)  : #文件是否存在
+        # 已经下载成功
+        progressBar(('%s-%s') % (index,name),100,100)
+        return
+    #把下载地址发送给requests模块
+    r = requests.get(download_url)
+    total_size = 0
+    try:
+        header_str = r.headers['Content-Length']
+        if header_str:
+            total_size = int(header_str)
+    except BaseException as e:
+        writelog(download_url,name,e)
+        return
+    temp_size = 0
+    # ZIP的application/x-zip-compressed     
+    # RAR的application/octet-stream  
+    content_type = r.headers['Content-Type']
+    suffix = 'rar'
+    if 'application/x-zip-compressed' in content_type:
+        suffix = 'zip'
+   
+    #下载文件
+    with open(getPptFilePathName(dirNmae,name,suffix),"wb") as f:
+        if total_size == 0 :
+            f.write(r.content)
+            progressBar(('%s-%s') % (index,name),100,100)
+        else:
+            for chunk in r.iter_content(chunk_size=1024):
+                if chunk:
+                    temp_size += len(chunk)
+                    f.write(chunk)
+                    f.flush()
+                    progressBar(('%s-%s') % (index,name),total_size,temp_size)
+                   
+
+
+
+
+isGetDownloadUrl = False
 
 if __name__ == '__main__':
 
     sqliteUtil = SqliteUtil()
-    ppt_51_url_list = get_ppt_51_page_list()
-    print('总共发现%s个页面' % len(ppt_51_url_list))
-    for index, pageUrl in enumerate(ppt_51_url_list):
-        onePageDownloadInfoList = []
-        onePageDetailsInfoList = getOnePageDetailsInfoList(pageUrl,index)
-        for i, item in enumerate(onePageDetailsInfoList):
-            # 用描述地址去查询是否已经有下载地址
-            downloadUrl = sqliteUtil.selectDownloadUrl(item.detailsUrl)
-            if downloadUrl.strip() == '':
-                # 获取下载地址
-                downloadUrl = getDownloadUrl(getDownloadPageUrl(item.detailsUrl))
-            onePageDownloadInfoList.append(PptUrlInfo(item.typeStr,item.name,item.detailsUrl,downloadUrl))
-            progressBar(('第%s页获取下载地址'% (index+1)),len(onePageDetailsInfoList), ( i+1 ))
-        sqliteUtil.insert_url_info(onePageDownloadInfoList)
 
-    # os.system('cls')
+    if isGetDownloadUrl:
+        ppt_51_url_list = get_ppt_51_page_list()
+        print('总共发现%s个页面' % len(ppt_51_url_list))
+        for index, pageUrl in enumerate(ppt_51_url_list):
+            onePageDownloadInfoList = []
+            onePageDetailsInfoList = getOnePageDetailsInfoList(pageUrl,index)
+            for i, item in enumerate(onePageDetailsInfoList):
+                # 用描述地址去查询是否已经有下载地址
+                downloadUrl = sqliteUtil.selectDownloadUrl(item.detailsUrl)
+                if downloadUrl.strip() == '':
+                    # 获取下载地址
+                    downloadUrl = getDownloadUrl(getDownloadPageUrl(item.detailsUrl))
+                onePageDownloadInfoList.append(PptUrlInfo(item.typeStr,item.name,item.detailsUrl,downloadUrl))
+                progressBar(('第%s页获取下载地址'% (index+1)),len(onePageDetailsInfoList), ( i+1 ))
+            sqliteUtil.insert_url_info(onePageDownloadInfoList)
+
+        # os.system('cls')
+        
+        print()
+
     downloadInfoLis = sqliteUtil.select_url_info_all()
-    print()
     print('总共发现%s个模板' % len(downloadInfoLis))
+
+    # 初始化下载
+    baseDowloadDir = getBaseSaveDir()
+    for index ,item in enumerate(downloadInfoLis):
+        saveDir = jointPath(baseDowloadDir,item.typeStr)
+        makedirs(saveDir)
+        downloadFile(index+1, saveDir,item.name,item.downloadUrl)
+        print()
+
+
 
 
 
